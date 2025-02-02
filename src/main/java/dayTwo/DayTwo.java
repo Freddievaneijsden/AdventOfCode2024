69d37fe7e6f7fcd02630a7c113e274922b8d530f
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-02 19:25:34 +0100

    Read data from txt

--- src/main/java/dayTwo/DayTwo.java
+++ src/main/java/dayTwo/DayTwo.java
@@ -1,4 +1,32 @@
 package dayTwo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DayTwo {
+
+    public static void main(String[] args) {
+        final String dayTwoInput = "test.txt";
+        List<String> input = new ArrayList<>();
+
+        try {
+            File myObj = new File(dayTwoInput);
+            Scanner reader = new Scanner(myObj);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                input.add(data);
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not Found.");
+            e.printStackTrace();
+        }
+
+
+
+        input.forEach(System.out::println);
+    }
 }
