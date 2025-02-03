67819164749c5c76a39c5917f35f7708822e2b98
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-03 10:21:48 +0100

    Convert List<List<String>> to List<Integer>

--- src/main/java/dayTwo/DayTwo.java
+++ src/main/java/dayTwo/DayTwo.java
@@ -5,28 +5,65 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class DayTwo {
 
     public static void main(String[] args) {
-        final String dayTwoInput = "test.txt";
-        List<String> input = new ArrayList<>();
+        final String textFile = "test.txt";
+        List<String> dayTwoInput = new ArrayList<>();
+        List<List<String>> reportListString = new ArrayList<>();
 
+        scanEachLineToList(textFile, dayTwoInput);
+        splitStringBySpaceAndSaveInListOfLists(dayTwoInput, reportListString);
+
+        List<List<Integer>> reportList = convertListOfListsWithStringToInteger(reportListString);
+
+        reportList.forEach(System.out::println);
+        System.out.println(reportList.getFirst().getFirst() + reportList.get(0).get(1));
+    }
+
+    private static void splitStringBySpaceAndSaveInListOfLists(List<String> dayTwoInput, List<List<String>> reportListString) {
+        for (String s : dayTwoInput) {
+            List<String> singleReportListString;
+            List<String> singleReportList = new ArrayList<>();
+            singleReportListString = List.of(s.split("\\s+"));
+            for (int i = 0; i < singleReportListString.size(); i++) {
+//                singleReportList.add(Integer.parseInt(singleReportListString.get(i)));
+                  singleReportList.add(singleReportListString.get(i));
+            }
+            reportListString.add(singleReportList);
+        }
+    }
+
+    private static void scanEachLineToList(String textFile, List<String> dayTwoInput) {
         try {
-            File myObj = new File(dayTwoInput);
+            File myObj = new File(textFile);
             Scanner reader = new Scanner(myObj);
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                input.add(data);
+                dayTwoInput.add(data);
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not Found.");
             e.printStackTrace();
         }
+    }
+
+    private static List<List<Integer>> convertListOfListsWithStringToInteger(List<List<String>> reportListString) {
+        List<List<Integer>> reportList = new ArrayList<>();
+        List<Integer> singleReportList;
+        for (int i = 0; i < reportListString.size(); i++) {
+            singleReportList = reportListString.get(i).stream()
+                    .map(Integer::parseInt)
+                    .toList();
+            reportList.add(singleReportList);
+        }
+        return reportList;
+    }
 
 
-
-        input.forEach(System.out::println);
+    private static boolean increasingLevelsOrNot(List<List<Integer>> reportListString) {
+        return true;
     }
 }
