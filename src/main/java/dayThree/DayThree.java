c5ca5b2c8951cf741b36aae4f6d857ffb8822355
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-02-05 12:56:26 +0100

    Add separatePairs

--- src/main/java/dayThree/DayThree.java
+++ src/main/java/dayThree/DayThree.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,21 +16,42 @@ public class DayThree {
 
     public static void main(String[] args) {
         final String textFile = "dayThreeInput.txt";
-        String dayThreeInput = "";
+        String dayThreeInput = fileToString(textFile);
+
+        List<String> uncorruptedMul = new ArrayList<>(addMatchToList(dayThreeInput));
+        List<int[]> mappedNumbers = new ArrayList<>(separatePairs(uncorruptedMul));
+
+        int total = 0;
+        for (int i = 0; i < mappedNumbers.size(); i++) {
+            int[] pair = mappedNumbers.get(i);
+            int firstNumber = pair[0];
+            int secondNumber = pair[1];
+            total += firstNumber * secondNumber;
+        }
+
+        System.out.println(total);
+    }
+
+    public static List<int[]> separatePairs(List<String> listString){
+        List<int[]> mappedNumbers = new ArrayList<>();
+        for (String pair : listString) {
+            String[] parts = pair.split(",");
+            int firstNumber = Integer.parseInt(parts[0].trim());
+            int secondNumber = Integer.parseInt(parts[1].trim());
+            mappedNumbers.add(new int[]{firstNumber, secondNumber});
+        }
+        return mappedNumbers;
+    }
+
+    public static List<String> addMatchToList (String input) {
         List<String> uncorruptedMul = new ArrayList<>();
-
-
-        dayThreeInput = fileToString(textFile);
-//        System.out.println(dayThreeInput);
-
         Pattern pattern = Pattern.compile("mul\\(([0-9]*,[0-9]*)\\)");
-        Matcher matcher = pattern.matcher(dayThreeInput);
+        Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
             uncorruptedMul.add(matcher.group(1));
         }
-
-        uncorruptedMul.forEach(System.out::println);
+        return uncorruptedMul;
     }
 
     public static String fileToString(String p) {
